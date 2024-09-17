e8e8f5001b4029fb8db60eca589a8a5307b3bbae
Jonas <65494730+jonaswer@users.noreply.github.com>
2024-09-17 08:39:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,137 +1,111 @@
 <?php
+// Database configuration
+$dbhost = '<your hostname>';
+$dbuser = '<database user>';
+$dbpass = '<database password>';
+$dbname = '<database name>';
 
-$dbhost = "<your hostname>";
-$dbuser = "<select dabase user>";
-$dbpass = "<database passwort>";
-$db = "<database name>";
+// Create a new MySQLi connection
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-
-if ($conn)
-{
-        //echo "connected";
+// Check connection
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
 }
 
-$query = "SELECT * FROM DataLake"; //your table name
+// Prepare and execute SELECT query
+$query = 'SELECT ID, timestamp, temperature, humidity FROM DataLake';
+$result = $mysqli->query($query);
 
-$sqlQuery = mysqli_query($conn, $query);
-
-if (!$sqlQuery)
-{
-        echo "SQL query error";
+if (!$result) {
+    die('Query error: ' . $mysqli->error);
 }
 
-$query2 = "DELETE FROM DataLake WHERE timestamp  < (NOW() - INTERVAL 2 DAY)"; //your table name
-
-$sqlQuery2 = mysqli_query($conn, $query2);
-
-if (!$sqlQuery2) {
-        echo "SQL query2 error";
+// Fetch data and process it
+$dataArray = [];
+while ($row = $result->fetch_assoc()) {
+    $dataArray[] = [
+        'ID'          => $row['ID'],
+        'timestamp'   => $row['timestamp'],
+        'temperature' => (float) $row['temperature'],
+        'humidity'    => (float) $row['humidity'],
+    ];
 }
 
-class DataSet
-{
-        public $dataArray = array();
-        public $lastData = array();
-        public $xAxis = array();
-        public $tempArray = array();
-        public $humiArray = array();
+// Get the last data entry
+$lastData = end($dataArray);
 
-        function __construct($sqlQuery){
-                while ($row = mysqli_fetch_array($sqlQuery)){
-                        $dataArray[]=array($row["ID"],$row["timestamp"],$row["temperature"],$row["humidity"]); //ID and timestamp is optional, in case its part the database table
-                        $this->dataArray = $dataArray;
-                        }
-                $this->lastData = end($this->dataArray);
-                $this->tempArray = array_column($this->dataArray, '2');
-                $this->humiArray = array_column($this->dataArray, '3');
-                $this->xAxis = array_column($this->dataArray, '0');
+// Extract temperature and humidity arrays
+$tempArray = array_column($dataArray, 'temperature');
+$humiArray = array_column($dataArray, 'humidity');
 
-                }
+// Close the result set
+$result->free();
 
-        function getData(){
-                return $this->dataArray;
-        }
-        function getTemp(){
-                return $this->tempArray;
-        }
-        function getHumi(){
-                return $this->humiArray;
-        }
-        function getLastData(){
-                return $this->lastData;
-        }
-
+// Prepare and execute DELETE query to remove old data
+$deleteQuery = 'DELETE FROM DataLake WHERE timestamp < (NOW() - INTERVAL 2 DAY)';
+if (!$mysqli->query($deleteQuery)) {
+    die('Delete query error: ' . $mysqli->error);
 }
 
-$Data = new DataSet($sqlQuery);
-$conn -> close();
-
+// Close the database connection
+$mysqli->close();
 ?>
 
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
-<script>
-window.onload = function () {
-
-//temperature chart
-var data = <?php echo json_encode($Data->getTemp(), JSON_NUMERIC_CHECK); ?>;
-data = data.map(function (row, index) {
-    return {
-        x: index,
-        y: row
-    };
-});
-
-var chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-        text: "Temperature"
-    },
-    axisY: {
-        title: "°C"
-    },
-    data: [{
-        type: "line",
-        dataPoints: data
-    }]
-});
-
-chart.render();
-
-//humidity chart
-var data2 = <?php echo json_encode($Data->getHumi(), JSON_NUMERIC_CHECK); ?>;
-data2 = data2.map(function (row, index) {
-    return {
-        x: index,
-        y: row
-    };
-});
-
-var chart2 = new CanvasJS.Chart("chartContainer2", {
-    title: {
-        text: "Humidity"
-    },
-    axisY: {
-        title: "[%]"
-    },
-    data: [{
-        type: "line",
-        dataPoints: data2
-    }]
-});
-
-chart2.render();
-
-}
-
-</script>
-
+    <meta charset="UTF-8">
+    <title>Temperature and Humidity Charts</title>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </head>
 <body>
-<div id="chartContainer" style="height: 250px; width: 50%;"></div>
-<div id="chartContainer2" style="height: 250px; width: 50%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<div> Letzte Datenaufnahme um: <?php print_r($Data->getLastData()[1]) ?> </div>
+    <div id="chartContainerTemp" style="height: 250px; width: 50%;"></div>
+    <div id="chartContainerHumi" style="height: 250px; width: 50%;"></div>
+    <div>Last Data Recorded At: <?php echo htmlspecialchars($lastData['timestamp']); ?></div>
+
+    <script>
+        window.onload = function () {
+            // Temperature chart data
+            var tempDataPoints = <?php echo json_encode(array_values($tempArray), JSON_NUMERIC_CHECK); ?>;
+            var tempData = tempDataPoints.map(function (value, index) {
+                return { x: index, y: value };
+            });
+
+            var tempChart = new CanvasJS.Chart("chartContainerTemp", {
+                title: {
+                    text: "Temperature"
+                },
+                axisY: {
+                    title: "°C"
+                },
+                data: [{
+                    type: "line",
+                    dataPoints: tempData
+                }]
+            });
+            tempChart.render();
+
+            // Humidity chart data
+            var humiDataPoints = <?php echo json_encode(array_values($humiArray), JSON_NUMERIC_CHECK); ?>;
+            var humiData = humiDataPoints.map(function (value, index) {
+                return { x: index, y: value };
+            });
+
+            var humiChart = new CanvasJS.Chart("chartContainerHumi", {
+                title: {
+                    text: "Humidity"
+                },
+                axisY: {
+                    title: "%"
+                },
+                data: [{
+                    type: "line",
+                    dataPoints: humiData
+                }]
+            });
+            humiChart.render();
+        };
+    </script>
 </body>
 </html>
